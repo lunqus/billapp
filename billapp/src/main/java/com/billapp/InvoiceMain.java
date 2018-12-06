276b11729d0c983b5904da77b17f97dfa7e18325
LUNKUS <sigitas@lunkus.com>
2018-12-06 11:23:09 +0200

    vcs

---
+++ billapp/src/main/java/com/billapp/InvoiceMain.java
@@ -0,0 +1,22 @@
+package com.billapp;
+
+public class InvoiceMain {
+
+    public class InvoiceMain {
+
+        public static void main(String[] args) {
+            // Prints "Hello, World" to the terminal window.
+
+            Invoice in = new Invoice();
+            Foe foe = new Foe();
+
+            foe.seller();
+            foe.customer();
+
+            System.out.println();
+
+            in.records();
+
+        }
+
+}
---
+++ billapp/src/main/java/var/Invoice.java
@@ -0,0 +1,38 @@
+package var;
+
+/**
+ *
+ * @author Sigitas Lunkus
+ *
+ */
+
+public class Invoice {
+
+    public void records() {
+
+        // picture logo;
+
+        char invoiceNum;
+
+        long invoiceIssueDate;
+        long invoicePayDate;
+
+        char operationName;
+        char operationCode;
+        String item;
+        String service;
+
+        float unitPrice;
+        float vat;
+        float discount;
+        String amountInWords;
+
+        item = "Mercedes Benz";
+        service = "Financial Services";
+
+
+        System.out.println( "Preke: " + item);
+        System.out.println( "Paslauga: " + service);
+
+    }
+}
