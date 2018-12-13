90eb3c017e3a5a4334dcd49698efef3f095d5756
LUNKUS <sigitas@lunkus.com>
2018-12-13 10:57:55 +0200

    last com

---
+++ billapp/src/main/java/com/billapp/InvoiceMain.java
@@ -0,0 +1,66 @@
+package com.billapp;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InvoiceMain {
+
+    public static void main(String[] args) {
+        // Prints Customer, Seller and Item or Service to the terminal window.
+
+        Company com = new Company();
+        // com.setInvoices(new ArrayList<>());
+        // com.getInvoices().add(new Invoice());
+
+        Partner part = new Partner();
+        part.setName("Akropolis");
+
+        Invoice inv = new Invoice();
+        inv.setSeller(part);
+        inv.setCustomer(new Partner());
+        inv.setInvoiceNum("First_1237");
+        inv.getItems().add(new Item("Kojines"));
+        inv.getItems().add(new Item("Kojines"));
+        com.getInvoices().add(inv);
+        com.getSellers().add(part); // -----------
+
+        inv = new Invoice();
+        part = new Partner();
+        part.setName("Rimi");
+        inv.setSeller(part);
+        inv.setCustomer(new Partner());
+        inv.setInvoiceNum("Second_563253");
+        inv.getItems().add(new Item("Pirstines"));
+        inv.getItems().add(new Item("Liemene"));
+        inv.getItems().add(new Item("Kelnes"));
+        com.getInvoices().add(inv);
+        com.getSellers().add(part); // -----------
+
+        inv = new Invoice();
+        part = new Partner();
+        part.setName("Maxima");
+        inv.setSeller(part);
+        inv.setCustomer(new Partner());
+        inv.setInvoiceNum("Third_87675");
+        inv.getItems().add(new Item("Kojines"));
+        inv.getItems().add(new Item("Megztinis"));
+        com.getInvoices().add(inv);
+        com.getSellers().add(part); // -----------
+
+
+        InvoiceSearch search = new InvoiceSearch();
+
+        List<Invoice> searchItem = search.searchByItem(com, "Kojines");
+        List<Invoice> searchSeller = search.searchBySeller(com, "Akropolis");
+        List<Invoice> searchCustomer = search.searchByCustomer(com, "Antanas");
+
+        for (Invoice invoice : searchItem) {
+            System.out.println(invoice.getInvoiceNum());
+        }
+
+
+    }
+
+}
+
