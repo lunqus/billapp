0d0ef864497279dee5bb1f9090b8f5b626f15ab4
LUNKUS <sigitas@lunkus.com>
2018-12-14 12:38:38 +0200

    test the best

---
+++ billapp/src/main/java/Tests/InvoiceSearchTest.java
@@ -0,0 +1,52 @@
+package Test;
+
+import com.billapp.Controller.InvoiceSearch;
+import com.billapp.Model.Company;
+import com.billapp.Model.Invoice;
+import com.billapp.Model.Item;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class InvoiceSearchTest {
+
+    Invoice inv = new Invoice();
+    Company com = new Company();
+
+    private final InvoiceSearch searchTest = new InvoiceSearch();
+    String itemName = "Kojines";
+
+    @Test
+    public void searchByItem(Company company, String itemName) {
+
+        List<Invoice> searchItem = new ArrayList<>();
+        //List<Invoice> searchItems = new ArrayList<>();
+
+        for (Invoice invoice : company.getInvoices()) {
+
+            for (Item item : invoice.getItems()) {
+
+                if (item.getName().contains(itemName)) {
+                    searchItem.add(invoice);
+
+                    break;
+                }
+
+            }
+        }
+        for (Invoice invoice : searchItem) {
+            System.out.println("Sąskaitos Nr. " + invoice.getInvoiceNum() + " " + "Prekės: ");
+            for (Item item : invoice.getItems()){
+                System.out.println(item.getName());
+            }
+
+    }
+
+
+    }
+
+}
