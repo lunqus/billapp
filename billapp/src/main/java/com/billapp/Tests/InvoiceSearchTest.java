49e78b4275ccd2393a2c4adf4f394c99fd8be3ad
LUNKUS <sigitas@lunkus.com>
2018-12-14 05:45:40 +0200

    test creation

---
+++ billapp/src/main/java/com/billapp/Tests/InvoiceSearchTest.java
@@ -0,0 +1,18 @@
+package com.billapp.Tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InvoiceSearchTest {
+
+    @org.junit.jupiter.api.Test
+    void searchByItem() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void searchBySeller() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void searchByCustomer() {
+    }
+}
