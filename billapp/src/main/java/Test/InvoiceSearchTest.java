e456da88435c4f88b67401befcae5ba0a68592ff
LUNKUS <sigitas@lunkus.com>
2018-12-14 06:49:35 +0200

    testing solution

--- billapp/src/main/java/Test/InvoiceSearchTest.java
+++ billapp/src/main/java/Test/InvoiceSearchTest.java
@@ -1,12 +1,18 @@
 package Test;
 
+import com.billapp.Controller.InvoiceSearch;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class InvoiceSearchTest {
 
+    private final InvoiceSearch searchTest = new InvoiceSearch();
+
     @Test
     public void searchByItem() {
+
+    assertArrayEquals(searchTest.searchByItem().contains("Kojines"));
+
     }
 }
